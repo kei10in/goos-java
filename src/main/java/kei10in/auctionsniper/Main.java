2762cde03013ec98ae98f91b184defa37ed0ee97
kei10in <kei10in@gmail.com>
2013-04-05 06:55:30 +0900

    refactoring

--- src/main/java/kei10in/auctionsniper/Main.java
+++ src/main/java/kei10in/auctionsniper/Main.java
@@ -11,7 +11,7 @@ import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 
-public class Main implements SniperListener {
+public class Main {
     private static final int ARG_HOSTNAME = 0;
     private static final int ARG_USERNAME = 1;
     private static final int ARG_PASSWORD = 2;
@@ -78,7 +78,8 @@ public class Main implements SniperListener {
 
         Auction auction = new XMPPAuction(chat);
         chat.addMessageListener(
-            new AuctionMessageTranslator(new AuctionSniper(auction, this)));
+            new AuctionMessageTranslator(
+                new AuctionSniper(auction, new SniperStateDisplayer())));
         auction.join();
     }
 
@@ -90,21 +91,26 @@ public class Main implements SniperListener {
             }
         });
     }
+    
+    
+    public class SniperStateDisplayer implements SniperListener {
 
-    public void sniperLost() {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                ui.showStatus(MainWindow.STATUS_LOST);
-            }
-        });        
-    }
+        public void sniperBidding() {
+            showStatus(MainWindow.STATUS_BIDDING);
+        }
 
-    public void sniperBidding() {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                ui.showStatus(MainWindow.STATUS_BIDDING);
-            }
-        });
+        public void sniperLost() {
+            showStatus(MainWindow.STATUS_LOST);
+        }
+
+        private void showStatus(final String status) {
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    ui.showStatus(status);
+                }
+            });
+        }
+    
     }
 
 }
