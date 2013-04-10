cfcc21038437ebed91c043ed0fcba45f2a66bc0b
kei10in <kei10in@gmail.com>
2013-04-10 21:26:12 +0900

    p.182: use auction.getItemId() method instead of itemId field

--- src/test/java/kei10in/test/endtoend/auctionsniper/ApplicationRunner.java
+++ src/test/java/kei10in/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -11,10 +11,8 @@ public class ApplicationRunner {
     private static final String SNIPER_PASSWORD = "sniper";
     private static final String XMPP_HOSTNAME = "localhost";
     private AuctionSniperDriver driver;
-    private String itemId;
 
     public void startBiddingIn(final FakeAuctionServer auction) {
-        itemId = auction.getItemId();
         Thread thread = new Thread("Test Application") {
             @Override
             public void run() {
@@ -38,27 +36,31 @@ public class ApplicationRunner {
             SnipersTableModel.textFor(SniperState.JOINING));
     }
     
-    public void hasShownSniperIsBidding(int lastPrice, int lastBid) {
+    public void hasShownSniperIsBidding(
+        FakeAuctionServer auction, int lastPrice, int lastBid) {
         driver.showsSniperStatus(
-            itemId, lastPrice, lastBid,
+            auction.getItemId(), lastPrice, lastBid,
             SnipersTableModel.textFor(SniperState.BIDDING));        
     }
 
-    public void showsSniperHasLostAution(int lastPrice, int lastBid) {
+    public void showsSniperHasLostAution(
+        FakeAuctionServer auction, int lastPrice, int lastBid) {
         driver.showsSniperStatus(
-            itemId, lastPrice, lastBid,
+            auction.getItemId(), lastPrice, lastBid,
             SnipersTableModel.textFor(SniperState.LOST));
     }
 
-    public void hasShownSniperIsWinning(int winningBid) {
+    public void hasShownSniperIsWinning(
+        FakeAuctionServer auction, int winningBid) {
         driver.showsSniperStatus(
-            itemId, winningBid, winningBid,
+            auction.getItemId(), winningBid, winningBid,
             SnipersTableModel.textFor(SniperState.WINNING));
     }
 
-    public void showsSniperHasWonAution(int lastPrice) {
+    public void showsSniperHasWonAution(
+        FakeAuctionServer auction, int lastPrice) {
         driver.showsSniperStatus(
-            itemId, lastPrice, lastPrice,
+            auction.getItemId(), lastPrice, lastPrice,
             SnipersTableModel.textFor(SniperState.WON));
     }
 
--- src/test/java/kei10in/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ src/test/java/kei10in/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -17,7 +17,7 @@ public class AuctionSniperEndToEndTest {
         auction.hasRecievedJoinRequestFromSniper(
             ApplicationRunner.SNIPER_XMPP_ID);
         auction.announceClosed();
-        application.showsSniperHasLostAution(0, 0);
+        application.showsSniperHasLostAution(auction, 0, 0);
     }
     
     @Test
@@ -30,11 +30,11 @@ public class AuctionSniperEndToEndTest {
         
         auction.reportPrice(1000, 98, "other bidder");
         
-        application.hasShownSniperIsBidding(1000, 1098);
+        application.hasShownSniperIsBidding(auction, 1000, 1098);
         auction.hasRecievedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
         
         auction.announceClosed();
-        application.showsSniperHasLostAution(1000, 1098);
+        application.showsSniperHasLostAution(auction, 1000, 1098);
     }
     
     @Test
@@ -47,15 +47,15 @@ public class AuctionSniperEndToEndTest {
         
         auction.reportPrice(1000, 98, "other bidder");
         
-        application.hasShownSniperIsBidding(1000, 1098);
+        application.hasShownSniperIsBidding(auction, 1000, 1098);
         
         auction.hasRecievedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
         
         auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
-        application.hasShownSniperIsWinning(1098);
+        application.hasShownSniperIsWinning(auction, 1098);
         
         auction.announceClosed();
-        application.showsSniperHasWonAution(1098);
+        application.showsSniperHasWonAution(auction, 1098);
     }
 
     @After
