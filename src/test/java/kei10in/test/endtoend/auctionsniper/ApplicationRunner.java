a2d0cc03ef18ff4be10ea15e31f5e0680a3358c2
kei10in <kei10in@gmail.com>
2013-04-11 23:36:03 +0900

    p.191: change test to use item id field and join button for joining auction

--- src/test/java/kei10in/test/endtoend/auctionsniper/ApplicationRunner.java
+++ src/test/java/kei10in/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -13,23 +13,13 @@ public class ApplicationRunner {
     private AuctionSniperDriver driver;
 
     public void startBiddingIn(final FakeAuctionServer ... auctions) {
-        Thread thread = new Thread("Test Application") {
-            @Override
-            public void run() {
-                try {
-                    Main.main(arguments(auctions));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        thread.setDaemon(true);
-        thread.start();
-        driver = new AuctionSniperDriver(1000);
-        driver.hasTitle(MainWindow.APPLICATION_TITLE);
-        driver.hasColumnTitles();
-        driver.showsSniperStatus(
-            SnipersTableModel.textFor(SniperState.JOINING));
+        startSniper();
+        for (FakeAuctionServer auction: auctions) {
+            final String itemId = auction.getItemId();
+            driver.startBiddingFor(itemId);
+            driver.showsSniperStatus(
+                itemId, 0, 0, SnipersTableModel.textFor(SniperState.JOINING));
+        }
     }
     
     public void hasShownSniperIsBidding(
@@ -66,14 +56,21 @@ public class ApplicationRunner {
         }
     }
     
-    private static String[] arguments(FakeAuctionServer... auctions) {
-        String[] arguments = new String[auctions.length + 3];
-        arguments[0] = XMPP_HOSTNAME;
-        arguments[1] = SNIPER_ID;
-        arguments[2] = SNIPER_PASSWORD;
-        for (int i = 0; i < auctions.length; i++) {
-            arguments[i + 3] = auctions[i].getItemId();
-        }
-        return arguments;
+    private void startSniper() {
+        Thread thread = new Thread("Test Application") {
+            @Override
+            public void run() {
+                try {
+                    Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.setDaemon(true);
+        thread.start();
+        driver = new AuctionSniperDriver(1000);
+        driver.hasTitle(MainWindow.APPLICATION_TITLE);
+        driver.hasColumnTitles();
     }
 }
--- src/test/java/kei10in/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ src/test/java/kei10in/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -5,14 +5,18 @@ import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 import static java.lang.String.valueOf;
 import static org.hamcrest.Matchers.equalTo;
 
+import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.table.JTableHeader;
 
 import kei10in.auctionsniper.ui.MainWindow;
 
 import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JButtonDriver;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.driver.JTableHeaderDriver;
+import com.objogate.wl.swing.driver.JTextFieldDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 public class AuctionSniperDriver extends JFrameDriver {
@@ -24,6 +28,26 @@ public class AuctionSniperDriver extends JFrameDriver {
                   showingOnScreen()),
                   new AWTEventQueueProber(timeoutMillis, 100));
     }
+    
+    public void startBiddingFor(String itemId) {
+        itemIdField().replaceAllText(itemId);
+        bidButton().click();
+    }
+    
+    @SuppressWarnings("unchecked")
+    private JTextFieldDriver itemIdField() {
+        JTextFieldDriver newItemId = new JTextFieldDriver(
+            this, JTextField.class, named(MainWindow.NEW_ITEM_ID_NAME));
+        newItemId.focusWithMouse();
+        return newItemId;
+    }
+    
+    @SuppressWarnings("unchecked")
+    private JButtonDriver bidButton() {
+        return new JButtonDriver(
+            this, JButton.class, named(MainWindow.JOIN_BUTTON_NAME));
+    }
+    
     
     @SuppressWarnings("unchecked")
     public void showsSniperStatus(String statusText) {
